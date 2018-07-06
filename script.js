a5df603cb632c8518e46f4bff1c48e4d15f4c33a
lubbe <Ludwig@Ludwigs-MBP.lan>
2018-07-07 00:55:28 +0200

    push

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // jquery side menu controller
-var keeper;
-$(document).ready(function(){
-	$('.nav-icon1').click(function(){
+$(document).ready(function () {
+    $('.nav-icon1').click(function () {
         $(this).toggleClass('open');
-	});
+    });
 });
 
-$(".hamburgerBtn").click(function(){
+$(".hamburgerBtn").click(function () {
     $(".sideMenuWrapper").toggle(200);
     $('.container2').toggleClass('push');
     $('.menu-type').toggleClass('open');
 });
 
-$(".sideMenuWrapper").click(function(e){
-    if(e.pageX > $( window ).width() * 0.75){
+$(".sideMenuWrapper").click(function (e) {
+    if (e.pageX > $(window).width() * 0.75) {
         $('.nav-icon1').removeClass('open');
         $('.container2').toggleClass('remove');
         $(".sideMenuWrapper").toggle(200);
     }
 });
+
