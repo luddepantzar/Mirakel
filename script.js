59151ee76476525cda1e0a72e365b41ac2833ea3
lubbe <Ludwig@Ludwigs-MBP.lan>
2018-07-06 14:49:28 +0200

    FIXED MENY + BURGERMEENY

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 });
 
 $(".hamburgerBtn").click(function(){
-    $(".sideMenuWrapper").toggle(500);
+    $(".sideMenuWrapper").toggle(200);
     $('.container2').toggleClass('push');
     $('.menu-type').toggleClass('open');
 });
@@ -16,6 +16,6 @@ $(".sideMenuWrapper").click(function(e){
     if(e.pageX > $( window ).width() * 0.75){
         $('.nav-icon1').removeClass('open');
         $('.container2').toggleClass('remove');
-        $(".sideMenuWrapper").toggle(500);
+        $(".sideMenuWrapper").toggle(200);
     }
 });
